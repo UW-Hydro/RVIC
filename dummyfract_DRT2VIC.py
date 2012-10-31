2f83b8a639e0e10ab306fe81637021531969fa99
Joe Hamman <jhamman@hydro.washington.edu>
2012-10-31 16:49:12 -0700

    change directory in/out paths for preprossing scripts

--- dummyfract_DRT2VIC.py
+++ dummyfract_DRT2VIC.py
@@ -15,19 +15,19 @@ args = parser.parse_args()
 #inputs = ['10th', '12th', '16th', '1', '2', '8th', 'half', 'qd']
 input= args.res 
 
-for i in input:
-    invar = 'DRT_' + i + '_FDR_globe.asc'
-      outvar = 'DRT_' + i + '_FDR_globe_vic.asc'
+#for i in input:
+#    invar = 'DRT_' + i + '_FDR_globe.asc'
+#    outvar = 'DRT_' + i + '_FDR_globe_vic.asc'
 
 #inpath = '/raid/jhamman/DRT/upscaled_global_hydrography/by_HydroSHEDS_Hydro1k/flow_direction/'
-           inpath = arg.inpath
+inpath = arg.inpath
 #outpath = '/raid/jhamman/DRT/upscaled_global_hydrography/by_HydroSHEDS_Hydro1k/flow_direction/VIC_flow_direction/'
 outpath =arg.outpath 
 
 d = {1: 1, 2: 1, 4: 1, 8: 1, 16: 1, 32: 1, 64: 1, 128: 1, -9999: arg.nodata}
 
   # open the input file (DRT flow direction)
-  f1 = inpath+invar
+  f1 = inpath
   drtdata = loadtxt(f1, skiprows = 6, dtype = int)
 
   # replace DRT values with VIC values
@@ -35,16 +35,16 @@ d = {1: 1, 2: 1, 4: 1, 8: 1, 16: 1, 32: 1, 64: 1, 128: 1, -9999: arg.nodata}
   for k, v in d.iteritems(): vicdata[drtdata==k] = v
 
   # save converted data to tmp file
-  with open(outpath+'temp/tmp', 'w') as ftemp:
-    savetxt(outpath+'temp/tmp', vicdata, fmt='%1.1i', delimiter=' ', newline='\n')
+  with open('./temp/tmp', 'w') as ftemp:
+    savetxt('./temp/tmp', vicdata, fmt='%1.1i', delimiter=' ', newline='\n')
 
   # get original header (minus no data line (6))
   with open(f1) as myfile:
       head=[myfile.next() for x in xrange(5)]
 
   # open output file
-  f2 = open(outpath+outvar, 'a+')
-  tmp = open(outpath+'temp/tmp', 'r')
+  f2 = open(outpath, 'a+')
+  tmp = open('./temp/tmp', 'r')
 
   #write header data
   for hh in head:
--- flowd_DRT2VIC.py
+++ flowd_DRT2VIC.py
@@ -27,9 +27,9 @@ args = parser.parse_args()
 #inputs = ['10th', '12th', '16th', '1', '2', '8th', 'half', 'qd']
 input= args.res
 
-for i in input:
-  invar = 'DRT_' + i + '_FDR_globe.asc'
-  outvar = 'DRT_' + i + '_FDR_globe_vic.asc'
+#for i in input:
+ #invar = 'DRT_' + i + '_FDR_globe.asc'
+ #outvar = 'DRT_' + i + '_FDR_globe_vic.asc'
 
   #inpath = '/raid/jhamman/DRT/upscaled_global_hydrography/by_HydroSHEDS_Hydro1k/flow_direction/'
   inpath = arg.inpath
@@ -39,7 +39,7 @@ for i in input:
   d = {1: 3, 2: 4, 4: 5, 8: 6, 16: 7, 32: 8, 64: 1, 128: 2, -9999: arg.nodata}
 
   # open the input file (DRT flow direction)
-  f1 = inpath+invar
+  f1 = inpath
   drtdata = loadtxt(f1, skiprows = 6, dtype = int)
 
   # replace DRT values with VIC values
@@ -47,16 +47,16 @@ for i in input:
   for k, v in d.iteritems(): vicdata[drtdata==k] = v
 
   # save converted data to tmp file
-  with open(outpath+'temp/tmp', 'w') as ftemp:
-    savetxt(outpath+'temp/tmp', vicdata, fmt='%1.1i', delimiter=' ', newline='\n')
+  with open('./temp/tmp', 'w') as ftemp:
+    savetxt('./temp/tmp', vicdata, fmt='%1.1i', delimiter=' ', newline='\n')
 
   # get original header (minus no data line (6))
   with open(f1) as myfile:
       head=[myfile.next() for x in xrange(5)]
 
   # open output file
-  f2 = open(outpath+outvar, 'a+')
-  tmp = open(outpath+'temp/tmp', 'r')
+  f2 = open(outpath, 'a+')
+  tmp = open('./temp/tmp', 'r')
 
   #write header data
   for hh in head:
