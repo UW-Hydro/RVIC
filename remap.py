94e4d3535e050d2ccaa0da1a5f9de019492ab9dc
Joe Hamman <jhamman@hydro.washington.edu>
2012-11-05 13:19:45 -0800

    debugging of remap script continues...

--- remap.py
+++ remap.py
@@ -5,9 +5,9 @@ import numpy as np
 import os
 import time
 
-infile='/Users/jhamman/Dropbox/RASM_Joe/YUKON.uh_s'
-nodata=-9999
-
+infile='/usr1/jhamman/RASM/vic_routing/route_rasm/arctic/UH_S_files/YUKON.uh_s'
+fill_value=-9999
+res=0.0625
 
 #Load the Header data for each point
 indata1 = open( infile, "r").readlines()[1::2]
@@ -21,62 +21,82 @@ uhs = np.array([x.split() for x in uhs], dtype='float')
 
 #reshape the uhs output
 #make individual arrays of lats, lons, cell nums and fracs
-lats=indata1[:,0]
-lons=indata1[:,1]
-frac=indata1[:,2]
-xcell=indata1[:,3]
-ycell=indata1[:,4]
+lat_in=indata1[:,0]
+lon_in=indata1[:,1]
+frac_in=indata1[:,2]
+xcell_in=indata1[:,3]
+ycell_in=indata1[:,4]
+
+xin=xcell_in-min(xcell_in)
+yin=ycell_in-min(ycell_in)
+
+xmin=min(xin)
+xmax=max(xin)
+ymin=min(yin)
+ymax=max(yin)
+
+xs = np.arange(xmin,xmax,res)
+ys = np.arange(ymin,ymax,res)
 
 # initialize the target arrays (uhs&fra-> a&b) with the nodata value
-a = np.zeros((len(lons),len(lats),uhs.shape[1]))+nodata
-b = np.zeros((lons.length,lats.length))+nodata
-
-xin=xcell-min(xcell)+1
-yin=ycell-min(ycell)+1
+#a = np.zeros((len(xs),len(ys),uhs.shape[1]))+fill_value
+b = np.zeros((1,len(xs),len(ys)))+fill_value
 
 #fill in values values
 for n, i in enumerate(xin):
-   b[xin[i],yin[i]] = fracin[i]
-   a[xin[i],yin[i],:] = uhs_in[:,i]
+   b[0,xin[i],yin[i]] = frac_in[i]
+   #a[xin[i],yin[i],j] = uhs_in[:,i]
 
 
    ############# WRITE TO NETCDF4 ##################
    
-# dimensions
-lon = f.createDimension('lon', xdim)
-lat = f.createDimension('lat', ydim)
-time = f.createDimension('time',tdim)
-print(f.dimensions)
+# initialize the netcdf file
+f = Dataset('test.nc', 'w', format='NETCDF4_CLASSIC')
+print(f.file_format)
 
-# variables
+
+# set dimensions
+time = f.createDimension('time', None)
+lon = f.createDimension('lon', (len(xs)))
+lat = f.createDimension('lat', (len(ys)))
+#print(f.dimensions)
+
+# initialize variables
+times = f.createVariable('time','f8',('time',))
 longitudes = f.createVariable('longitude','f4',('lon',))
 latitudes = f.createVariable('latitude','f4',('lat',))
-times = f.createVariable('time','i4',('time',))
-frac = f.createVariable('fraction','f8',('lon','lat','time'))
-uh = f.createVariable('unit_hydrograph','f8',('lon','lat','time'))
+frac = f.createVariable('fraction','f8',('lon','lat','time'),fill_value=fill_value)
+#uh = f.createVariable('unit_hydrograph','f8',('lon','lat','time'),fill_value=fill_value)
 
-#write attributes for netcdf
+import time
+# write attributes for netcdf
 f.description = 'test script'
-f.history = 'Created ' + create_t
-f.source = 'RASM routing program'
+f.history = 'Created ' + time.ctime(time.time())
+f.source = 'RASM routing program remap.py'
 latitudes.units = 'degrees north'
 longitudes.units = 'degrees east'
 times.units = 'Days'
 
-f.close()
+#f.close()
 
 #print attributes
-for name in f.ncattrs():
-   print 'Global attr', name, '=', getattr(f.name)
+#for name in f.ncattrs():
+#   print 'Global attr', name, '=', getattr(f.name)
 
 f = Dataset('test.nc','a')
 # data
-lon[:] = np.arange(xmin,xmax,res)
-lat[:] = np.arange(ymin,ymax,res)
-fracs[:] = b
-uhs[:] = a
+lons1 = np.arange(xmin,xmax,res)
+longitudes[:] = lons1
+latitudes[:] = np.arange(ymin,ymax,res)
+
+print np.shape(b)
+
+frac[:,:,:] = b[0,:,:]
+#uhs[:] = a
 
 f.close()
+
+print '*** SUCCESS writing tst.nc!'
 
 # remap the uhs.nc file using cdo
 #outfile = "outtest.nc"
