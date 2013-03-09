2ee999c41acf33865f96280a637ce63859fda174
Joe Hamman <jhamman@hydro.washington.edu>
2013-03-09 11:31:11 -0800

    in progress updates to convolution script

--- convolution/python/convolve.py
+++ convolution/python/convolve.py
@@ -3,80 +3,131 @@
 
 from netCDF4 import Dataset
 import numpy as np
+import time as tm
+import argparse
 
-basins = ['PECUT','OBSAL','LENKU','KOLKO','YENIG','MACRR','YUKPS','DVIUP']
-runs = ['run2','run3','run4','run5','run6','run7','run8']
-
-sims = ['r33RBVIC60', 'r33RBVIC70']
-
-grid_nc = '/nfs/hydro6/raid/nijssen/rasm/masks/racm_masks_121108.nc'
-f3 = Dataset(grid_nc)
-area  = f3.variables['DOMA_AREA'][:]
 
 re = 6.37122e6
-area = area[:,:]*re*re
-for sim in sims:
+area = area[:,:]**re
 
-    rasm_nc = '/nfs/thermal/raid/jhamman/RASM_results/r33/'+sim+'.vic.ha.1990s.qflux.nc'
-    f2 = Dataset(rasm_nc)
-    runoff = f2.variables['Runoff'][:]
-    baseflow = f2.variables['Baseflow'][:]
-    time1 = f2.variables['time'][:]
-    flux = runoff + baseflow
+def main():
+    grid_nc,area_var,flux_nc,flux_vars,uh_nc,uh_var = process_command_line()
+    q = convolve(grid_nc,area_var,flux_nc,flux_vars,uh_nc,uh_var)
+    write_netcdf()
 
-    for run in runs:
+def convolve(grid_nc,flux_nc,uh_nc):
+    Grid = read_netcdf(grid_nc)
+    Flux = read_netcdf(flux_nc)
+    UH = read_netcdf(uh_nc)
 
-        for basin in basins:
-            print 'starting basin', basin, ' in run ', run
-            # open the UH netcdf file
-            basin_nc ='/raid/jhamman/temp_uh_files/'+run+'/'+basin+'_RASM_UH.nc'
-            f1 = Dataset(basin_nc)
-            uh = f1.variables['unit_hydrograph'][:]
-            frac = f1.variables['fraction'][:]
-            t_uh = f1.variables['time'][:]
+    convolution():
 
-            q = np.zeros((len(flux)+len(t_uh)))
+    return q
+    
+    
+def process_command_line():
+    """
+    Parse arguments and assign flags for further loading of variables, for
+    information on input arguments, type rout.py -h
+    """
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("grid_nc", type=str, help="Input grid netcdf including area variable")
+    parser.add_argument("flux_nc", type=str, help="Input grid netcdf including runoff/baseflow fluxes")
+    parser.add_argument("uh_nc", type=str, help="Input unit hydrograph grid netcdf")
+    parser.add_argument("-av","--area_var", type=str, help="variable name for area",default='area')
+    parser.add_argument("-fv","--flux_vars", type=str, help="If only one var should be used in the convolution, input here",default=['runoff','baseflow'],nargs='+')
+    parser.add_argument("-uv", "--uh_var",type=str, help="variable name for unit hydrograph")
+    args = parser.parse_args()
 
-            #Do the convolution
-            for i in xrange(len(flux)):
-                for j in xrange(len(t_uh)):
-                    tstep = i+j
-                    q[tstep]= q[tstep] + np.sum(flux[i,:,:]*uh[j,:,:]*area[:,:]*frac[:,:])
+def write_netcdf(out_nc,q,time):
+    #Write to NC file
+    f = Dataset(out_nc, 'w', format='NETCDF4_CLASSIC')
+    
+    # set dimensions
+    time = f.createDimension('time', None)
+    
+    # initialize variables
+    time = f.createVariable('time','f8',('time',))
+    Q = f.createVariable('Q','f8',('time',))
+    
+    # write attributes for netcdf
+    f.description = 'Streamflow.'
+    f.history = 'Created ' + tm.ctime(tm.time())
+    f.source = 'RASM routing program - convolve'
+    
+    time.units = 'day'
+    time.description = 'Days since 1-1-1'
+    
+    Q.description = 'Daily runoff at station'
+    Q.units = 'm^3/s'
+    
+    # write data to variables initialized above
+    time[:] = time1
+    Q[:] = q[0:len(time1)]
+    
+    f.close()
+    return
+ 
+##################################################################################
+## Read netCDF Inputs
+## Read data from input netCDF.
+##################################################################################
+def read_netcdf(ncFile,vars = [],coords = False, verbose = False):
+    """
+    Read data from input netCDF. Will read all variables if none provided.
+    Will also return all variable attributes.
+    Both variables (data and attributes) are returned as dictionaries named by variable
+    """
+    if verbose: print 'Reading input data vars:', vars, ', from file:',ncFile
+    f = Dataset(ncFile,'r')
+    if vars==[]: vars = f.variables.keys()
+    d={}
+    a={}
+    if coords:
+        if isinstance(vars,str):
+            d[vars] = f.variables[vars][coords]
+            a[vars] = f.variables[vars].__dict__
+        else:
+            for var in vars:
+                d[var] = f.variables[var][coords]
+                a[var] = f.variables[var].__dict__
+    else:
+        if isinstance(vars,str):
+            d[vars] = f.variables[vars][:]
+            a[vars] = f.variables[vars].__dict__
+        else:
+            for var in vars:
+                d[var] = f.variables[var][:]
+                a[var] = f.variables[var].__dict__
+    f.close()
+    return d,a
 
-            #Change units to m3/sec
-            q = q[:]/1000/86400
+def convolution(flux,uh,area,frac,full = False):   
+    """
+    Do the convolution by looping over all uh and flux timesteps.
+    Return a 1d array that matches the time length of the flux variable.
+    If full = True, the length will be the combined length of the flux and uh.  
+    """
+    q = np.zeros((len(flux)+len(t_uh)))
 
-            #Write to NC file
-            out_nc = 'Streamflows/'+basin+'_'+run+'_'+sim+'_streamflow.nc'
-            f = Dataset(out_nc, 'w', format='NETCDF4_CLASSIC')
-            print(f.file_format)
+    #Do the convolution
+    for i in xrange(len(flux)):
+        for j in xrange(len(t_uh)):
+            tstep = i+j
+            q[tstep]= q[tstep] + np.sum(flux[i,:,:]*uh[j,:,:]*area[:,:]*frac[:,:])
+            
+    #Change units to m3/sec
+    q /= 1000/86400
 
-            # set dimensions
-            time = f.createDimension('time', None)
-            print(f.dimensions)
+    # Truncate if not true
+    if not Full:
+        q = q[:len(flux)]
 
-            # initialize variables
-            time = f.createVariable('time','f8',('time',))
-            Q = f.createVariable('Q','f8',('time',))
+    return q
 
-            import time as t
-            # write attributes for netcdf
-            f.description = 'Streamflow for RASM,created using UH file from '+run+' and fluxes from '+sim+'.'
-            f.history = 'Created ' + t.ctime(t.time())
-            f.source = 'RASM routing program - convolve'
-
-            time.units = 'day'
-            time.description = 'Days since 1-1-1'
-
-            Q.description = 'Daily runoff at station'
-            Q.units = 'm^3/s'
-
-            # write data to variables initialized above
-            time[:] = time1
-            Q[:] = q[0:len(time1)]
-
-            f.close()
-            print '*** SUCCESS writing streamflows for ',out_nc, '***'
-
-print 'Done'    
-
+##################################################################################
+# Run Program
+##################################################################################
+if __name__ == "__main__":
+    main()
