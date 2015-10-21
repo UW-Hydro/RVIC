bfd4092a8e7af005bbee079e9f4c9f67ad8a77a8
Joe Hamman <jhamman1@uw.edu>
2015-10-21 11:21:46 -0700

    skip cdo test on travis

--- tests/unit/test_remap.py
+++ tests/unit/test_remap.py
@@ -1,8 +1,13 @@
 import pytest
-from cdo import CDOException
+try:
+    from cdo import CDOException
+    cdo_unavailable = False
+except ImportError:
+    cdo_unavailable = True
 from rvic.core.remap import remap
 
 
+@pytest.mark.skipif(cdo_unavailable, reason='cdo not installed')
 def test_cdo_raises_exception():
     with pytest.raises(CDOException):
         remap('grid_file', 'in_file', 'out_file')
