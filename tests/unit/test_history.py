ed912dc0589b06b07ad68680d8a7dd91b0e479ec
Joe Hamman <jhamman1@uw.edu>
2015-11-05 13:33:59 -0800

    fix string comparison

--- tests/unit/test_history.py
+++ tests/unit/test_history.py
@@ -5,7 +5,8 @@ from rvic.core.variables import Rvar
 from rvic.core.history import Tape
 
 
-@pytest.mark.skipif(os.getenv('TRAVIS', False), reason='on travis')
+@pytest.mark.skipif(os.getenv('TRAVIS', 'false').lower() == 'true',
+                    reason='on travis')
 @pytest.fixture()
 def rvar(scope='function'):
     dirname = os.path.dirname(__file__)
@@ -15,7 +16,8 @@ def rvar(scope='function'):
     return rv
 
 
-@pytest.mark.skipif(os.getenv('TRAVIS', False), reason='on travis')
+@pytest.mark.skipif(os.getenv('TRAVIS', 'false').lower() == 'true',
+                    reason='on travis')
 def test_create_tape_instance(rvar):
     history_tape = Tape(1.25, 'test', rvar, grid_area=np.zeros((10, 11)),
                         outtype='array')
